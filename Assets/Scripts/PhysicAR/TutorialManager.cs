9c665bb5ea13b2207506db47e3fdb1f0d26985b8
anjelomerte <vknoben@t-online.de>
2025-07-22 11:09:32 +0200

    disabled delayed init of Vuforia Engine again

---
+++ Assets/Scripts/PhysicAR/TutorialManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    #region Properties
+
+    #endregion
+
+
+    #region Unity lifecycle
+
+    #endregion
+
+
+    #region Methods
+
+    // Callback for initializing tutorial logic
+    public void StartTutorial()
+    {
+
+    }
+
+    #endregion
+}
